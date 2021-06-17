606f85a353225476a8c9e61b4cc2831657c6a175
Jonas Lopes <jmdlopes.almeida@gmail.com>
2021-06-17 09:20:36 +0100

    Testando os testes

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,35 +10,22 @@ public class DateUtilsTest {
 	
 	
 	@Test
-	public void deveRetornarTrueParaDatasFuturas() {
-		
+	public void deveRetornarTrueParaDatasFuturas() {	
 		LocalDate date = LocalDate.of(2030, 01, 01);
-
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-		
-		
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));	
 	}
 	
 	@Test
 	public void deveRetornarFalseParaDatasPassadas() {
-		
 		LocalDate date = LocalDate.of(2010, 01, 01);
-
-		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
-		
-		
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));	
 	}
 	
 	@Test
 	public void deveRetornarTrueParaDataAtual() {
-		
 		LocalDate date = LocalDate.now();
-
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-		
-		
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));	
 	}
-	
 	
 
 }
